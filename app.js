dbd5bb2d91267510f91c22c1a5ac64e0891001be
Neveh Allon <nevehallon@gmail.com>
2018-03-18 18:28:32 +0200

    Feat: new 2d rendering context for painting

---
+++ app.js
@@ -0,0 +1,4 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+//creating the ctx variable to store the 2D rendering context —
+//the actual tool we can use to paint on the Canvas
