d266df28f193a773bd4e5a650e305bf8a525d030
Neveh Allon <nevehallon@gmail.com>
2018-03-18 19:27:37 +0200

    Feat: ball reacts when it hits a border

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 //creating the ctx variable to store the 2D rendering context —
@@ -6,11 +15,13 @@ let x = canvas.width / 2;
 let y = canvas.height - 30
 let dx = 2;
 let dy = -2;
+let ballRadius = 10;
+let ballColor = "#0095DD";
 
-function drawBall() {
+let drawBall = function() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
-  ctx.fillStyle = "#0095DD";
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = ballColor;
   ctx.fill();
   ctx.closePath();
 }
@@ -18,7 +29,18 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+    ballColor = `${getRandomColor()}`;
+  } //reverses the ball's trajectory when part of the ball touches SIDE borders
+
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+    ballColor = `${getRandomColor()}`;
+  }
+
   x += dx;
   y += dy;
-}
+} //reverses the ball's trajectory when part of the ball touches TOP AND BOTTUM borders
 setInterval(draw, 10);
