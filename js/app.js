2e1e25efbfceb0cbfe649fc9eee11e6826dd0311
Neveh Allon <nevehallon@gmail.com>
2018-03-18 18:47:56 +0200

    Ball for game added

--- js/app.js
+++ js/app.js
@@ -2,21 +2,11 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 //creating the ctx variable to store the 2D rendering context —
 //the actual tool we can use to paint on the Canvas
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+function draw() {
+  ctx.beginPath();
+  ctx.arc(50, 50, 10, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath(); // drawing code
+}
+setInterval(draw, 10);
