6036d89b79f1e1a197f4434bcd94e0ce7b732dd2
Neveh Allon <nevehallon@gmail.com>
2018-03-18 18:59:37 +0200

    Cleaned up JS

--- js/app.js
+++ js/app.js
@@ -7,14 +7,17 @@ let y = canvas.height - 30
 let dx = 2;
 let dy = -2;
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // this rectangle will be cleared of any content previously painted there
+function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, 10, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
-  ctx.closePath(); // drawing code
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
   x += dx;
   y += dy;
 }
