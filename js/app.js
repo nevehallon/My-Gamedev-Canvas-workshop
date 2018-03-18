9fd94fdb77f7d8c5150b2190c35510373a6b26ab
Neveh Allon <nevehallon@gmail.com>
2018-03-18 18:51:54 +0200

    Making ball move

--- js/app.js
+++ js/app.js
@@ -2,11 +2,18 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 //creating the ctx variable to store the 2D rendering context —
 //the actual tool we can use to paint on the Canvas
+let x = canvas.width / 2;
+let y = canvas.height - 30
+let dx = 2;
+let dy = -2;
+
 function draw() {
   ctx.beginPath();
-  ctx.arc(50, 50, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, 10, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath(); // drawing code
+  x += dx;
+  y += dy;
 }
 setInterval(draw, 10);
